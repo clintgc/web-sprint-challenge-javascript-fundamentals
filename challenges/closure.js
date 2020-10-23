12cd422eb82d22e35549327fd59a3e42aba0b8b5
Clint GC <clint.gordoncarroll@gmail.com>
2020-10-23 15:29:24 -0500

    sprint challenge completed.

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,10 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// Because the nestedFunction() has closure in the myFunction and has lexical scope to use the variable internal defined inside myFunction()
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+const summation = (n) => (n * (n + 1)) / 2;
+console.log(summation(4));
